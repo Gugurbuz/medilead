fcc1e98618824158305efb4aaf659a25f41e1568
Gugurbuz <gurkangurbuz@hotmail.com.tr>
2025-12-06 11:55:11 +0300

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import { Toaster } from '@/components/ui/toaster';
 import PatientDashboard from '@/components/PatientDashboard';
 
